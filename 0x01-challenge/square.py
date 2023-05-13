a2258351fe188194d52bf806af900a1275d15634
lil-armstrong <oebong1@gmail,com>
2023-05-13 14:48:06 +0100

    refactor task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,27 +1,23 @@
-#!/usr/bin/python3
-""" square class module """
+#!/usr/bin/env python3
 class Square:
-    """ square class """
-    width = 0
-    height = 0
     
     def __init__(self, **kwargs):
-        """ initialize a square instance """
         self.width = kwargs.get('width', 0)
         self.height = kwargs.get('height', 0)
+    
     @property
     def area(self):
-        """ Area of the square """
+        """Area of the square"""
         return self.width * self.width
+    
     @property
     def perimeter(self):
-        """ calculate perimeter of square """
+        """Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
-
+    
     def __str__(self):
-        """ string represemtation square instance """
         return f"{self.width}/{self.height}"
-
+    
 if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
