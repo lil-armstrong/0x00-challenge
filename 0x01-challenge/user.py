2a8c4c996bf68b125e557903b714e079846d6530
lil-armstrong <oebong1@gmail,com>
2023-05-13 14:35:24 +0100

    task 2

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -6,22 +6,22 @@ User class
 
 class User():
     """ Documentation """
-
+    
     def __init__(self):
         """ Documentation """
-        self.__email = None
-
+        self.__email = ""
+        
+    @property
+    def email(self):
+        """ Documentation """
+        return self.__email
+    
     @email.setter
     def email(self, value):
         """ Documentation """
         if type(value) is not str:
             raise TypeError("email must be a string")
         self.__email = value
-
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
 
 
 if __name__ == "__main__":
