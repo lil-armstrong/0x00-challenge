fda7ad94396b16971105d69554176db30882a9e5
lil-armstrong <oebong1@gmail,com>
2023-05-13 14:42:41 +0100

    refactor task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
 """ square class ,odule """
-class square():
+class Square():
     """ square class """
     width = 0
     height = 0
-
     
     def __init__(self, *args, **kwargs):
         """ initialize a square instance """
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        self.width = kwargs.get('width', 0)
+        self.height = kwargs.get('height', 0)
 
-    def area_of_my_square(self):
+    def area(self):
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def perimeter(self):
         """ calculate perimeter of square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """ string represemtation square instance """
-        return "{}/{}".format(self.width, self.height)
+        return f"{self.width}/{self.height}"
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.area)
+    print(s.perimeter)
